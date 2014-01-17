d5ea78aa93037ec919973b82094fc82d0b097dfd
buckholz <glenn.buckholz@covers.com>
2014-01-17 13:41:57 -0500

    missed comma

--- hangmaninstaller/modules/apache/manifests/init.pp
+++ hangmaninstaller/modules/apache/manifests/init.pp
@@ -5,7 +5,7 @@ class apache {
   }
   file {
     "/etc/httpd/conf.d/proxy_ajp.conf":
-      content => template("apache/proxy_ajp.conf.erb")
+      content => template("apache/proxy_ajp.conf.erb"),
       require => Package["httpd"]
   }
 }

bfc882f819b42cdea7a170fc97de438ecce815a7
buckholz <glenn.buckholz@covers.com>
2014-01-18 07:58:06 -0500

    output change

--- hangmaninstaller/signed.sh
+++ hangmaninstaller/signed.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SIGNED=""
-while [ -z "$SIGNED" ] ; do SIGNED=`curl -k -H "Accept: pson" https://puppetmaster.darkseer.org:8140/production/certificate_status/$1 | grep signed` ; echo checking DB - Exists:$1; sleep 5; done
+while [ -z "$SIGNED" ] ; do SIGNED=`curl -k -H "Accept: pson" https://puppetmaster.darkseer.org:8140/production/certificate_status/$1 | grep signed` ; echo checking tomcat - Exists:$1; sleep 5; done
 FACTS=""
-while [ -z "$FACTS" ] ; do FACTS=`curl -k -H "Accept: yaml" https://puppetmaster.darkseer.org:8140/production/facts/$1 | grep ipaddress:` ; echo checking DB:$1 - intip; sleep 5; done
+while [ -z "$FACTS" ] ; do FACTS=`curl -k -H "Accept: yaml" https://puppetmaster.darkseer.org:8140/production/facts/$1 | grep ipaddress:` ; echo checking tomcat:$1 - intip; sleep 5; done
 FACTS=""
-while [ -z "$FACTS" ] ; do FACTS=`curl -k -H "Accept: yaml" https://puppetmaster.darkseer.org:8140/production/facts/$1 | grep ec2_public_ipv4:` ; echo checking DB:$1 - extip; sleep 5; done
+while [ -z "$FACTS" ] ; do FACTS=`curl -k -H "Accept: yaml" https://puppetmaster.darkseer.org:8140/production/facts/$1 | grep ec2_public_ipv4:` ; echo checking tomcat:$1 - extip; sleep 5; done
 

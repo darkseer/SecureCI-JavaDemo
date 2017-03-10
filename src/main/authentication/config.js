905507757ac81871e3329212fe418fa57c97329c
James Caple <capable2@gmail.com>
2017-03-09 23:48:04 -0500

    Adding authentication example code to facilitate front-end development and Cognito User Pool Configuration in AWS environment

---
+++ src/main/authentication/config.js
@@ -0,0 +1,22 @@
+
+/*******************************************************************************
+ * Configuration information for the Dev Technology AWS Cognito User Pools.
+ * This will need to be updated to point to the Coveros AWS Environment.
+ * Author: James Caple, 3/9/2017
+ * Copyright (C) Dev Technology Group, Inc., All rights reserved
+ *******************************************************************************/
+
+module.exports = {
+	TICSConfig: {
+	 	config: {
+			region: 'us-east-1',
+	    	identityPoolId: 'us-east-1:0ad49232-e0db-4717-bf08-3e2ca35507c1',
+	    	UserPoolId: 'us-east-1_qQDx6p5SR',
+	    	ClientId: '584a9pau7pmmie35d3t9mv1c4s',
+	    	apiUrl: 'https://4jhbs3q7mf.execute-api.us-east-1.amazonaws.com/Dev',
+	    	Username: 'jcaple',
+	    	Password: '@TICSEmail69',
+	    	Email: ''
+	  	}
+	}
+}
